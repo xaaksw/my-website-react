fa10d390d9d8456dce1118808dbfdddea795aa1f
ahmedAshrefsw <ahmed.ashref.sw@gmail.com>
2021-08-20 04:03:15 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 
 import './App.css';
 
-import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   window.location.replace('https://www.linktree.com/ahmedashref');
-  // return (
+  return (
 
-    
-  //   // <Router>
-  //   //   <div className="App">
 
-  //   //     <Switch>
-  //   //       <Route path="/">
-  //   //           <h1>HI</h1>
-  //   //       </Route>
-  //   //     </Switch>
-  //   //   </div>
-  //   // </Router>
-  // );
+
+    <div className="App">
+      <h1> this website is under dev ! </h1>
+    </div>
+
+
+
+  );
 }
 
 export default App;
