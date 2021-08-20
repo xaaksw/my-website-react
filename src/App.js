3775a24c242aaba76aa8284777859077e7677ae9
ahmedAshrefsw <ahmed.ashref.sw@gmail.com>
2021-08-20 03:36:44 +0200

    initial and redirect to about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  window.location.replace('https://www.linktree.com/ahmedashref');
+  // return (
+
+    
+  //   // <Router>
+  //   //   <div className="App">
+
+  //   //     <Switch>
+  //   //       <Route path="/">
+  //   //           <h1>HI</h1>
+  //   //       </Route>
+  //   //     </Switch>
+  //   //   </div>
+  //   // </Router>
+  // );
 }
 
 export default App;
