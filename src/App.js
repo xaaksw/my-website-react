b87373daa83e7c5b79198ce27d088f1477f96f96
ahmed ashref <ahmedashref@ahmeds-MacBook-Pro.local>
2022-03-25 23:43:21 +0200

    refactor app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 // import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-
-  window.location.replace('https://www.linktree.com/ahmedashref');
-  return (
-
-
-
-    <div className="App">
-      <h1> This website is under development ! </h1>
-      
-    </div>
-
-
-
-  );
+  window.location.replace("https://www.linktree.com/ahmedashref");
+  return <HomePage></HomePage>;
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="header">
+      <h1> This website is under development ! </h1>
+    </div>
+  );
+}
+
+export default HomePage;
