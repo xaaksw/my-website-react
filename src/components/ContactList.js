7beae4d743c00624e9a35429651100eab5c65b63
ahmed ashref <ahmedashref@ahmeds-MacBook-Pro.local>
2022-03-27 02:32:53 +0200

    extract links to diffrent file

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,7 +1,8 @@
+import { contactLinks } from "../contactLinks";
 import ContactListItem from "./ContactListItem";
 
 function ContactList() {
-  let ContactListItems = links.map((link) => (
+  let ContactListItems = contactLinks.map((link) => (
     <ContactListItem name={link.name} url={link.url}></ContactListItem>
   ));
   return (
@@ -10,30 +11,4 @@ function ContactList() {
     </ul>
   );
 }
-
-const links = [
-  {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/ahmed-ashref/",
-  },
-  { name: "Twitter", url: "https://twitter.com/ahmedashrefsw" },
-  { name: "Withus بالعربي", url: "https://www.youtube.com/c/WithUss" },
-  {
-    name: "AIO - All In One product",
-    url: "https://www.facebook.com/allMaterialInOnePlace",
-  },
-  {
-    name: "Personal Youtube",
-    url: "https://www.youtube.com/channel/UCnqrTQ-YViVPYx5maCLvrew",
-  },
-  { name: "FacebookPage", url: "https://www.facebook.com/a4raff/" },
-  { name: "Instgram", url: "https://www.instagram.com/ahmedashrefsw/" },
-  { name: "GitHub", url: "https://github.com/ahmedAshrefsw" },
-  {
-    name: "SIC - sharing is caring project",
-    url: "https://www.facebook.com/ahmed.ashref.sw/posts/4039328866165623",
-  },
-  { name: "Alog podcast", url: "https://anchor.fm/a-log" },
-  { name: "MspCast", url: "https://anchor.fm/mspcast" },
-];
 export default ContactList;
